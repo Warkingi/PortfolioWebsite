52c0b05499017febddf8d16e74938f1042899a01
Warkingi <ncracknell.1@gmail.com>
2026-07-12 13:58:53 -0400

    Updated some text and added UEFN portal link to bottom.

--- JavaScript/projectDropdown.js
+++ JavaScript/projectDropdown.js
@@ -20,42 +20,6 @@ function closeProjectContent(article)
             iframe.contentWindow.postMessage(
                 '{"event":"command","func":"pauseVideo","args":""}', '*'
             );
-            window.console.log("CONTENT WINDOW");
         }
     });
-
-    // window.console.log("Closing Entry");
 }
-
-// function onClickProjectContent(event)
-// {
-//   CurrentElement = event.currentTarget
-//   const isAlreadyOpen = CurrentElement.classList.contains('is-expanded');
-
-//   // ── Accordion mode: close everything first ──────────
-//   // Delete these 4 lines if you want multiple cards
-//   // open at the same time.
-//   // cards.forEach(c => {
-//   //   c.classList.remove('is-expanded');
-//   //   c.setAttribute('aria-expanded', 'false');
-//   // });
-
-//   // ── Open the clicked card (if it was closed) ────────
-//   if (!isAlreadyOpen) {
-//     window.console.log("Hello");
-//     CurrentElement.classList.add('is-expanded');
-//     CurrentElement.setAttribute('aria-expanded', 'true');
-
-//     // Gently scroll card into view once animation begins
-//     setTimeout(() => {
-//       CurrentElement.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-//     }, 80);
-//   }
-//   else
-//   {
-//     CurrentElement.classList.remove('is-expanded')
-//     CurrentElement.setAttribute('aria-expanded', 'false');
-
-//     window.console.log("Goodbye");
-//   }
-// }
